2b181ebe51c164b8869e3ae5ea42bebdbce1c6d8
Adriano Raiano <adriano@raiano.ch>
2021-09-10 16:33:23 +0200

    cookie consent, ga and other cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,29 @@ import { useAnalytics } from '@/lib/analytics';
 
 import { appWithTranslation } from 'next-i18next'
 
+import CookieConsent from 'react-cookie-consent';
+
+const cookieConsentName = 'cookieconsentStatus';
+
 function App({ Component, pageProps }) {
-  useAnalytics();
+  const enable = useAnalytics(cookieConsentName);
 
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <CookieConsent
+        enableDeclineButton
+        location="bottom"
+        buttonText="Sure man!!"
+        cookieName={cookieConsentName}
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={360}
+        onAccept={() => enable()}
+      >
+        This website uses cookies to enhance the user experience.{" "}
+        <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+      </CookieConsent>
     </ThemeProvider>
   );
 }
