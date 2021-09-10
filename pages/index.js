ecbecbb909b1f4ff08f467ebbe64804e96196614
Adriano Raiano <adriano@raiano.ch>
2021-09-10 15:18:49 +0200

    redirect

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
-import i18nextConfig from '../next-i18next.config'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import languageDetector from '../lib/languageDetector';
+import { useRedirect } from '../lib/redirect';
 
-export const getStaticProps = () => {
-  const { locales, defaultLocale } = i18nextConfig.i18n
-  locales.splice(locales.indexOf(defaultLocale), 1)
-  locales.push(defaultLocale)
-  return {
-    props: {
-      locales,
-    },
-  }
-}
-
-const Index = ({ locales }) => {
-  const router = useRouter()
-
-  // language detection
-  useEffect(() => {
-    const detectedLng = languageDetector.detect()
-    for (const locale of locales) {
-      // eslint-disable-next-line no-undef
-      if (detectedLng.startsWith(locale)) {
-        languageDetector.cacheUserLanguage(detectedLng)
-        router.replace('/' + locale)
-        return
-      }
-    }
-  }, [])
-
+const Index = () => {
+  useRedirect()
   return <></>
 }
 
--- pages/second-page.js
+++ pages/second-page.js
@@ -1,37 +1,8 @@
-import i18nextConfig from '../next-i18next.config'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import languageDetector from '../lib/languageDetector';
+import { useRedirect } from '../lib/redirect';
 
-export const getStaticProps = () => {
-  const { locales, defaultLocale } = i18nextConfig.i18n
-  locales.splice(locales.indexOf(defaultLocale), 1)
-  locales.push(defaultLocale)
-  return {
-    props: {
-      locales,
-    },
-  }
-}
-
-const Index = ({ locales }) => {
-  const router = useRouter()
-
-  // language detection
-  useEffect(() => {
-    // without path detection
-    const detectedLng = languageDetector.detect(['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag'])
-    for (const locale of locales) {
-      // eslint-disable-next-line no-undef
-      if (detectedLng.startsWith(locale)) {
-        languageDetector.cacheUserLanguage(detectedLng)
-        router.replace('/' + locale + router.route)
-        return
-      }
-    }
-  }, [])
-
+const Redirect = () => {
+  useRedirect(/* optional to */)
   return <></>
 }
 
-export default Index
+export default Redirect
