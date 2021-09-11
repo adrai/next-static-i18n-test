0affdc0ea66a4549b0cb1de13a2e18494d57ce75
Adriano Raiano <adriano@raiano.ch>
2021-09-11 08:37:15 +0200

    simplify getStaticPaths, getStaticProps

--- pages/[locale]/index.js
+++ pages/[locale]/index.js
@@ -1,15 +1,11 @@
 import { useMemo } from 'react';
 import { getMDXComponent } from 'mdx-bundler/client';
-import { getLocaleFile } from '@/lib/mdx';
 import components from '@/components/MDXComponents';
 import Container from '@/components/Container';
 import { StaticI18nLink } from '@/components/StaticI18nLink';
-import { getI18nPaths } from '@/lib/getI18nPaths'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { getStaticPaths, makeStaticProps } from '@/lib/getStatic'
+import getSlug from '@/lib/getSlug'
 import { useTranslation } from 'next-i18next'
-import i18nextConfig from '../../next-i18next.config' // used as current next-i18next workaround for next.js > v10.2
-
-const slug = (new URL(import.meta.url).pathname.split("/").pop()).slice(0, -3)
 
 export default function Test({ code, frontMatter }) {
   const Component = useMemo(() => getMDXComponent(code), [code]);
@@ -50,19 +46,5 @@ export default function Test({ code, frontMatter }) {
   );
 }
 
-export const getStaticPaths = () => ({
-  fallback: false,
-  paths: getI18nPaths(),
-})
-
-export async function getStaticProps(ctx) {
-  return {
-    props: {
-      // if using markdown
-      ...await getLocaleFile(ctx?.params?.locale, slug),
-
-      // if using i18next here in react code
-      ...await serverSideTranslations(ctx?.params?.locale, ['common', 'footer'], i18nextConfig),
-    }
-  };
-}
+const getStaticProps = makeStaticProps(getSlug(import.meta.url));
+export { getStaticPaths, getStaticProps };
--- pages/[locale]/third-page.js
+++ pages/[locale]/third-page.js
@@ -1,14 +1,10 @@
 import { useMemo } from 'react';
 import { getMDXComponent } from 'mdx-bundler/client';
-import { getLocaleFile } from '@/lib/mdx';
 import components from '@/components/MDXComponents';
 import Container from '@/components/Container';
-import { getI18nPaths } from '@/lib/getI18nPaths'
-// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { getStaticPaths, makeStaticProps } from '@/lib/getStatic'
+import getSlug from '@/lib/getSlug'
 // import { useTranslation } from 'next-i18next'
-// import i18nextConfig from '../../next-i18next.config' // used as current next-i18next workaround for next.js > v10.2
-
-const slug = (new URL(import.meta.url).pathname.split("/").pop()).slice(0, -3)
 
 export default function Test({ code, frontMatter, isInRequestedLanguage }) {
   const Component = useMemo(() => getMDXComponent(code), [code]);
@@ -29,19 +25,5 @@ export default function Test({ code, frontMatter, isInRequestedLanguage }) {
   );
 }
 
-export const getStaticPaths = () => ({
-  fallback: false,
-  paths: getI18nPaths(),
-})
-
-export async function getStaticProps(ctx) {
-  return {
-    props: {
-      // if using markdown
-      ...await getLocaleFile(ctx?.params?.locale, slug),
-
-      // // if using i18next here in react code
-      // ...await serverSideTranslations(ctx?.params?.locale, ['common', 'footer'], i18nextConfig),
-    }
-  };
-}
+const getStaticProps = makeStaticProps(getSlug(import.meta.url));
+export { getStaticPaths, getStaticProps };
